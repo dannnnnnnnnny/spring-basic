3a13798eaa004eb2c8ef61a656ecbad29c78e99f
ehdgnl5249 <ehdgnl5249@naver.com>
2021-11-16 22:25:50 +0900

    ADD: spring container, spring bean annotation

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -9,30 +9,30 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+// 설정 정보 담당
+@Configuration
 public class AppConfig {
 
+    @Bean // 스프링 컨테이너에 등록됨
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
     }
 
-    private MemberRepository memberRepository() {
+    @Bean
+    public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
-    private DiscountPolicy discountPolicy() {
-//        return new FixDiscountPolicy();
-//        할인 정책 변경이 구성역할 담당인 AppConfig만 변경하면 됨 (사용영역인 OrderServiceImpl 코드 변경 X)
+    @Bean
+    public DiscountPolicy discountPolicy() {
         return new RateDiscountPolicy();
     }
 }
-
-/*
-    구성 영역은 당연히 변경됨
-    구성 역할을 담당하는 AppConfig는 애플리케이션이라는 공연의 기획자
-    공연 기획자는 참여자의 구현 객체들을 모두 알아야 함!
-* */
--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -4,13 +4,18 @@ import hello.core.AppConfig;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
 
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
-//        MemberService memberService = new MemberServiceImpl();
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+
+        // AppConfig의 @Bean 설정 정보를 스프링 컨테이너에 담아서 관리해줌
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class); // 기본적으로 Method명으로 등록이 되기 때문, 2번째 인자는 반환 타입
 
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
@@ -21,3 +26,7 @@ public class MemberApp {
         System.out.println("findMember = " + findMember.getName());
     }
 }
+
+/*
+    스프링은 대부분 ApplicationContext 로 시작함. (스프링 컨테이너라고 보면 됨 (@Bean 등 관리해줌))
+ */
